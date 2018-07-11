02a6e19c1d07ab2bb384aed81adb5751b7d4ce8c
Law Yang <lawtouayang@gmail.com>
2018-07-11 00:21:58 -0500

    material ui list for formation page

--- src/components/FormationDisplay/FormationDisplay.js
+++ src/components/FormationDisplay/FormationDisplay.js
@@ -6,7 +6,8 @@ import './formationDisplay.css';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 
 const mapStateToProps = state => ({
@@ -60,33 +61,48 @@ class FormationDisplay extends Component {
 
     render(){
         return(
-            <div>
-                <div className="formationName">
+            <div className="grid-container">
+                <div className="grid-item"></div>   
+                <div className="grid-item"></div>   
+ 
+                <div className="grid-item" id="formationName">
+                    <Paper elevation={1}>
                     <List component="nav">
                         {this.state.nameArr.map((name) => 
-                            <ListItem button>
+                            <ListItem key={name.id} button>
                                 <ListItemText primary={name.formation_name} />
                                 {/* <ListItemText primary="Trash" /> */}
                             </ListItem>
                         )}
                     </List>
+                    </Paper>
                 </div>
-
-                <div>
-                    <ul>
-                        {this.state.formationArr.map((detail) => 
-                            <div key={detail.id}>
-                                <li>Strengths: {detail.strengths}</li>
-                                <li>Weaknesses: {detail.weaknesses}</li>
-                                <li>Notes: {detail.notes}</li>
-                            </div>
-                        )}
-                    </ul>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item">
+                    <Paper elevation={1}>
+                        <Typography variant="headline" component="h3">
+                            Formation Details
+                        </Typography>
+                            <ListItem>
+                                {this.state.formationArr.map((detail) => 
+                                    <ListItemText primary={detail.strength} />)}
+                            </ListItem>
+                            <ul>
+                                {this.state.formationArr.map((detail) => 
+                                    <ul key={detail.id}>
+                                        <li>Strengths: {detail.strengths}</li>
+                                        <li>Weaknesses: {detail.weaknesses}</li>
+                                        <li>Notes: {detail.notes}</li>
+                                    </ul>
+                                )}
+                            </ul>
+                    </Paper>
                 </div>
-
-
-
-
+                <div className="grid-item"></div>   
+                <div className="grid-item"></div>   
+                <div className="grid-item"></div>   
+                <div className="grid-item"></div>  
             </div>
         )
     }
