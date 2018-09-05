0bf9e5694931346a3e893f005a1d2df968c94158
Law Yang <lawtouayang@gmail.com>
2018-09-05 11:31:22 -0500

    update to heroku

--- server/server.js
+++ server/server.js
@@ -30,10 +30,6 @@ app.use('/api/footy', footyRouter);
 // Serve static files
 app.use(express.static('build'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-})
-
 // App Set //
 const PORT = process.env.PORT || 5000;
 
